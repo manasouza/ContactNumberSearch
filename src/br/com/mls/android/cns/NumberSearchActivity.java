06816eee0bc0134b5f41aa6afddecfa6bbae638b
Manassés Souza <manasses.inatel@gmail.com>
2012-08-17 15:29:29 -0300

    Contact search updates

--- src/br/com/mls/android/cns/NumberSearchActivity.java
+++ src/br/com/mls/android/cns/NumberSearchActivity.java
@@ -1,8 +1,12 @@
 package br.com.mls.android.cns;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class NumberSearchActivity extends Activity {
 
@@ -10,6 +14,18 @@ public class NumberSearchActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.number_search);
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        if (cursor.getCount() > 0) {
+        	while (cursor.moveToNext()) {
+        		ListView listView = (ListView) findViewById(R.id.listView1);
+        		String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        		String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+        		if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+        			//Query phone here.  Covered next
+        		}
+        	}
+        }
     }
 
     @Override
