6336ae4107a686b5c7fde5b05cfddb7a1476eb2c
Manassés Souza <manasses.inatel@gmail.com>
2012-08-20 15:36:28 -0300

    *Layout updates *Showing contact names

--- gen/br/com/mls/android/cns/R.java
+++ gen/br/com/mls/android/cns/R.java
@@ -15,13 +15,16 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int editText1=0x7f070000;
-        public static final int listView1=0x7f070002;
-        public static final int menu_settings=0x7f070003;
+        public static final int editText1=0x7f070002;
+        public static final int listView1=0x7f070004;
+        public static final int menu_settings=0x7f070005;
+        public static final int textView=0x7f070003;
+        public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070001;
     }
     public static final class layout {
-        public static final int number_search=0x7f030000;
+        public static final int contact_list=0x7f030000;
+        public static final int number_search_form=0x7f030001;
     }
     public static final class menu {
         public static final int number_search=0x7f060000;
--- src/br/com/mls/android/cns/NumberSearchActivity.java
+++ src/br/com/mls/android/cns/NumberSearchActivity.java
@@ -1,11 +1,15 @@
 package br.com.mls.android.cns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class NumberSearchActivity extends Activity {
@@ -13,18 +17,26 @@ public class NumberSearchActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.number_search);
+        setContentView(R.layout.number_search_form);
         ContentResolver cr = getContentResolver();
         Cursor cursor = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
         if (cursor.getCount() > 0) {
+        	List<String> contactList = new ArrayList<String>();
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.contact_list, R.id.textView1);
+    		ListView listView = (ListView) findViewById(R.id.listView1);
         	while (cursor.moveToNext()) {
-        		ListView listView = (ListView) findViewById(R.id.listView1);
         		String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
         		String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
         		if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
         			//Query phone here.  Covered next
+        			
+//        			TextView tv2 = (TextView) findViewById(R.id.textView2);
+//        			tv2.setText(id);
+        			
+        			adapter.add(name);
         		}
         	}
+        	listView.setAdapter(adapter);
         }
     }
 
