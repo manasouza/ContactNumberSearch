39336f81437aac6b799d2f47351192c8855a14a5
Manassés Souza <manasses.inatel@gmail.com>
2016-04-21 18:49:19 -0300

    sync workspace

--- gen/br/com/mls/android/cns/R.java
+++ gen/br/com/mls/android/cns/R.java
@@ -16,11 +16,10 @@ public final class R {
     }
     public static final class id {
         public static final int etPhoneNumber=0x7f070002;
-        public static final int listView1=0x7f070004;
-        public static final int textView=0x7f070003;
+        public static final int listView1=0x7f070003;
         public static final int textView1=0x7f070000;
         public static final int textView2=0x7f070001;
-        public static final int update_contacts=0x7f070005;
+        public static final int update_contacts=0x7f070004;
     }
     public static final class layout {
         public static final int contact_list=0x7f030000;
