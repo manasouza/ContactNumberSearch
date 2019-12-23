5b6b53f0b5d5b2920e9857f5d6c621357cf707d2
Manassés Loiola de Souza <manasses.inatel@gmail.com>
2019-12-22 22:43:44 -0300

    [#60] upgrade target SDK + AndroidX

--- app/src/test/java/br/com/mls/contactnumbersearch/NumberSearchActivityTest.java
+++ app/src/test/java/br/com/mls/contactnumbersearch/NumberSearchActivityTest.java
@@ -8,7 +8,7 @@ import android.database.Cursor;
 import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import org.junit.Before;
